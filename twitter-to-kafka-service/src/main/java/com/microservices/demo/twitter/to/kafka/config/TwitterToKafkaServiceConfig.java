85ecf2f07c8a6ae439c8a9708e7607e80c2fdadf
LAPTOP-1RT688NU\tarab <tara.thapa@securin.io>
2024-02-22 13:49:32 +0545

    removal of unwanted import

--- twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/config/TwitterToKafkaServiceConfig.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/config/TwitterToKafkaServiceConfig.java
@@ -1,7 +1,6 @@
 package com.microservices.demo.twitter.to.kafka.config;
 
 import lombok.Data;
-import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
