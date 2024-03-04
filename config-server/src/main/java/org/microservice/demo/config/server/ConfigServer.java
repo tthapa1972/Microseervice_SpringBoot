40d771d833e0ebfad582b6bd601beb5a6e5bc3cd
LAPTOP-1RT688NU\tarab <tara.thapa@securin.io>
2024-03-04 18:58:15 +0545

    Using a common logback file for all microservices

---
+++ config-server/src/main/java/org/microservice/demo/config/server/ConfigServer.java
@@ -0,0 +1,12 @@
+package org.microservice.demo.config.server;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServer {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
